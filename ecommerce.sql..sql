c67573868e30e8eb1ff622479411f668420e4a2f
Barno June <barnoj21@gmail.com>
2025-04-23 19:12:58 +0300

    product and product category tables query

--- ecommerce.sql..sql
+++ ecommerce.sql..sql
@@ -20,8 +20,22 @@
 
 
 -- Table: product_category
+CREATE TABLE product_category (
+    category_id INT AUTO_INCREMENT PRIMARY KEY,
+    category_name VARCHAR(100) UNIQUE
+    );
 
 -- Table: product
+CREATE TABLE product (
+    product_id INT AUTO_INCREMENT PRIMARY KEY,
+    product_name VARCHAR(100),
+    brand VARCHAR(100),
+    base_price DECIMAL(10, 2),
+    category_id INT,
+    FOREIGN KEY (category_id) REFERENCES product_category(category_id)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE
+        );
 
 -- Table: product_variation
 
